4116bd633e6c53635e53d71fbf6e7ec100e920a6
luisjhc <ljoaquincuenca@gmail.com>
2022-02-14 17:14:50 +0100

    deploying

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import express from "express";
-import config from './config';
-import morgan from 'morgan';
-import cors from 'cors';
+import config from "./config";
+import morgan from "morgan";
+import cors from "cors";
 
-import videoRoutes from './routes/videosRoutes';
+import videoRoutes from "./routes/videosRoutes";
 
 const app = express();
 
 app.set("port", config.PORT);
-
-app.use(morgan('dev'));
+app.set("trust proxy", 1);
+app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 app.use(videoRoutes);
 
